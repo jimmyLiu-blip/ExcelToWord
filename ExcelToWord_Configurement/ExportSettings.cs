3f0cf2ffcc1ee747874a0ae7d9f5b2ffde8d4733
星驛 <a0978262702@gmail.com>
2025-10-26 12:01:41 +0800

    Initial commit

---
+++ ExcelToWord_Configurement/ExportSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelToWord_Configurement
+{
+    public class ExportSettings
+    {
+    }
+}
